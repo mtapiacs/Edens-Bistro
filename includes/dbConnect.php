c375fa9fc010d091cdd726ceaf9d44011322141a
Miguel Tapia <mtapiafdez@gmail.com>
2020-05-29 19:56:34 -0400

    Adding template files

---
+++ includes/dbConnect.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "dbConfig.php";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once "./includes/dbConnect.php"; ?>
+
+<h1>Hello planet</h1>
+
+<?php
+$conn->close();
+?>
