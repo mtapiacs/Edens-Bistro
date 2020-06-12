09ed4e27a9a3a9b2ed4d7a547abd6c095e92bfe5
Miguel Tapia <mtapiafdez@gmail.com>
2020-06-11 20:54:24 -0400

    Simple navigation

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/main.css" />
     <title>Eden's Bistro</title>
 </head>
 
 <body>
+    <nav class="navigation">
+        <ul>
+            <li><a class="navigation__main" href=" #">Eden's Bistro</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Reservation</a></li>
+            <li><a href="#">Pickup</a></li>
+        </ul>
+    </nav>
--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 include_once "./includes/header.php";
 ?>
 
-dfasdfsadfsdfsdf
+<div class="container">
+    <h3>Font: <span class="color-rubik">Rubik</span></h3>
+    <h3>Main Color: <span class="color-primary">#034c3c</span></h3>
+    <h3>Secondary Color: <span class="color-secondary"> #FFFFFF</span></h3>
+    <h3>Final Color: <span class="color-final">#902923</span></h3>
+</div>
 
 <?php
 include_once "./includes/footer.php";
