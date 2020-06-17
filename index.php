e53ddf8fd5880f04162f15a2e1d8b214f1d8d607
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-06-17 18:56:33 -0400

    changed back to original

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once "./includes/header.php";
 ?>
 
 <div class="container">
-    <h3>hello: <span class="color-rubik">Rubik Font</span></h3>
+    <h3>Font: <span class="color-rubik">Rubik Font</span></h3>
     <h3>Main Color: <span class="color-primary">#034c3c</span></h3>
     <h3>Secondary Color: <span class="color-secondary"> #FFFFFF</span></h3>
     <h3>Final Color: <span class="color-final">#902923</span></h3>
