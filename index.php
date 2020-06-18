9be9ade8d9774b33a0e5b9987976b1abb18b0bf9
Miguel Tapia <mtapiafdez@gmail.com>
2020-06-17 21:43:01 -0400

    Change

--- index.php
+++ index.php
@@ -3,11 +3,7 @@ include_once "./includes/header.php";
 ?>
 
 <div class="container">
-<<<<<<< HEAD
     <!-- <h3>Font: <span class="color-rubik">Rubik</span></h3>
-=======
-    <h3>Font: <span class="color-rubik">Rubik Font</span></h3>
->>>>>>> e53ddf8fd5880f04162f15a2e1d8b214f1d8d607
     <h3>Main Color: <span class="color-primary">#034c3c</span></h3>
     <h3>Secondary Color: <span class="color-secondary"> #FFFFFF</span></h3>
     <h3>Final Color: <span class="color-final">#902923</span></h3> -->
