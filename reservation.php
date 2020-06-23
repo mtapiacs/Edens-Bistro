a7be6edbaff507c0cb17baebf78530b02caa58f3
Miguel Tapia <mtapiafdez@gmail.com>
2020-06-23 18:26:12 -0400

    Images and reservation script

---
+++ reservation.php
@@ -0,0 +1,11 @@
+<?php
+include_once "./includes/header.php";
+?>
+
+<main class="main-container">
+    <h2 class="page-header">Reservation</h2>
+</main>
+
+<?php
+include_once "./includes/footer.php";
+?>
