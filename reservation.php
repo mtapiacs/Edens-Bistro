0b29c4ce7e38a75cbd4025d06fef4dcbc40cd42b
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-06-26 00:22:32 -0400

    changes to classes for bootstrap and then i added a collapse map
    The map might need improvement for a better look and feel. I am trying to find a good floor plan editor that doesnt have water mark and expects charges.

--- reservation.php
+++ reservation.php
@@ -3,34 +3,56 @@ include_once "./includes/header.php";
 ?>
 
 <main class="main-container">
+    
     <h2 class="page-header">Make A Reservation</h2>
-    <div class="reservation_form">
-        <form method="POST" action="">
-            <label class="reservatrion_labels">Name:</label>
-            <input type="text" name="registration_name"><br><br>
-
-            <label class="reservation_labels">Choose a room:</label>
-            <select name="reservation_rooms">
-                <option value="40seat">40 seat party room</option>
-                <option value="newlywed">Newlywed Corner</option>
-                <option value="15seat">15 seat party room</option>
-            </select> <br> <br>
-
-            <label class="reservation_labels">Number of People:</label>
-            <input type="number" name="amount_people" max="40"> <br> <br>
-
-            <label class="reservation_labels">Time:</label>
-            <input type="time" name="time_reservation"> <br> <br>
-
-            <label class="reservation_labels">Phone Number:</label>
-            <input type="tel" name="reservation_phone_number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"> <br> <br>
-
-            <label class="reservation_labels">Additional Instructions:</label>
-            <input type="textarea" name="time_reservation"> <br> <br>
-
-            <button class="btn btn-site-main" type="submit" value="registration_submit" name="registration_submit">Submit</button>
-        </form>
+<!--collapse was found from https://getbootstrap.com/docs/3.4/javascript/#collapse -->
+    <div class="container">        
+        <button class="btn btn-sm" type="button" data-toggle="collapse" data-target="#reservation_map" aria-expanded="false" aria-controls="reservation_map">Bistro Layout</button>
+        <div class="collapse" id="reservation_map"> <br>
+        <div class = text-center>
+        <img src="./img/reservation-map.jpg" class="d-block w-50" alt="Map of bistro" style="display: none">
+        </div>
     </div>
+    
+    <form method="POST" action="" class="form-container mx-auto mb-4">
+
+    <div class="form-group">
+        <label for="Name">Name:</label>
+        <input type="text" class="form-control" id="reservation_name" name = "reservation_name"> 
+    </div>
+
+    <div class="form-group">
+        <label for="reservation_room">Choose a room:</label>
+        <select name="reservation_rooms" class = "form-control">
+          <option value="40seat">40 seat party room</option>
+          <option value="newlywed">Newlywed Corner</option>
+          <option value="15seat">15 seat party room</option>
+         </select>
+    </div>
+
+    <div class="form-group">
+        <label for="amount_of_people">Amount of people:</label>
+        <input type="number" class="form-control" id="amount_of_people" max = "40"  name = "amount_of_people">
+    </div>
+
+    <div class="form-group">
+        <label for="time_of_reservation">Time:</label>
+        <input type="time" class="form-control" id="reservation_time" max = "40"  name = "reservation_time">
+        <small>Time Format: 12:30 PM</small>
+    </div>
+    
+    <div class="form-group">
+        <label for="reservation_phonenumber">Phonenumber:</label>
+        <input type="tel" class="form-control" id="reservation_phonenumber" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder = "123-456-7890" name = "reservation_phonenumber">
+    </div>
+
+    <div class="form-group">
+        <label for="reservation_comment_ques">Additional comments & Questions:</label>
+        <textarea type="text" class="form-control" id="reservation_questions_comments"  name = "reservation_questions_comments"></textarea>
+    </div>
+
+    <button class="btn btn-block btn-site-main" name="reservation-form" type="submit">Submit</button>
+    </form>
 </main>
 
 <?php
