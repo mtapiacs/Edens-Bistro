7be593777c1722ebb066cfef22a5a77561f01e0c
Stephanie M. Tea <st893@cairn.edu>
2020-06-28 19:26:47 -0400

    making a commit statement

--- menu.php
+++ menu.php
@@ -2,9 +2,7 @@
 include_once "./includes/header.php";
 ?>
 
-<main class="main-container">
-    <h2 class="page-header">Menu</h2>
-</main>
+<p> Hi </p>
 
 <?php
 include_once "./includes/footer.php";
