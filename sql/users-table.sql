47bee9d35f651b860f5ed236b52d413b6a7d9594
Miguel Tapia <mtapiafdez@gmail.com>
2020-06-28 20:14:37 -0400

    Adding users-table.sql

---
+++ sql/users-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(10) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+   	username VARCHAR(255) NOT NULL UNIQUE
+);
