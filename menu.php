1c7e57188ca2e863133f201f768be43b82b703e9
Stephanie M. Tea <st893@cairn.edu>
2020-07-02 15:14:52 -0400

    Working on the menu

--- menu.php
+++ menu.php
@@ -4,7 +4,15 @@ include_once "./includes/header.php";
 
 <main class="main-container">
     <h2 class="page-header">Menu</h2>
-    The menu
+    <div class="sidenav">
+      <a href="#">All Items</a>
+      <a href="#">Breakfast</a>
+      <a href="#">Lunch </a>
+      <a href="#">Dinner</a>
+      <a href="#">Sides</a>
+      <a href="#">Desserts</a>
+      <a href="#">Drinks</a>
+    </div>
 </main>
 
 <?php
