9319be6c9a4c2d5e9568202ed976bba78bebdfa7
Miguel Tapia <mtapiafdez@gmail.com>
2020-07-09 16:49:10 -0400

    Renaming

--- sql/event-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE events (
-	event_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    event_name VARCHAR(100) NOT NULL,
-    event_desc VARCHAR(255) NOT NULL,
-    event_time TIME NOT NULL,
-    event_date DATE NOT NULL
-);
