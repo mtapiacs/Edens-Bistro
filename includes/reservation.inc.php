235e3b37801209e3d1b5be1a15db36cd3eba07fe
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-07-18 14:47:42 -0400

    echoed a hello to make sure file worked when form was submitted
    Taking out the ht file did allow it to work correctly

--- includes/reservation.inc.php
+++ includes/reservation.inc.php
@@ -1,7 +1,12 @@
+<!-- <?php
+// include_once "header.php";
+?> -->
 <?php
 if(isset($_POST['reservation_submit'])){
     require "dbConnect.php";
     echo"hello";
 }
-
 ?>
+<!-- <?php
+// include_once "footer.php";
+?> -->
