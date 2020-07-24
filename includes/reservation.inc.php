01b5b05bc92821221cb2ebceebb0a6c8ab85f007
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-07-24 11:54:37 -0400

    sql statements for form
    Keep getting the error that there is a forgein key issue.
    
    Read that you need to place the foreign key values in first and sometimes it submits other times it doesnt. Idk im still looking into it

--- includes/reservation.inc.php
+++ includes/reservation.inc.php
@@ -12,8 +12,11 @@ if(isset($_POST['reservation_submit'])){
     $phonenum = $_POST['reservation_phonenumber'];
     $comments = $_POST['reservation_questions_comments'];
 
-    $sql = "INSERT INTO reservations (`name`, `email`, `room`, `num_people`,`reservation_time`, `reservation_date`,`phone_number`,`comments_questions`)
-            VALUES ($name,$email,$room,$num_people,$time,$date,$phonenum,$comments);";
+    $sql = "INSERT INTO rooms(`room_name`, `room_desc`, `room_capacity`) VALUES ($room,'description',$num_people);
+    
+            INSERT INTO reservations (`name`, `email`, `room`, `num_people`,`reservation_time`, `reservation_date`,`phone_number`,`comments_questions`)
+            VALUES ($name,$email,$room,$num_people,$time,$date,$phonenum,$comments);
+            )";
     mysqli_query($conn, $sql);
 
     header("Location: ../reservation.php?reservation=success");
