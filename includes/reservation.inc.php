a65a4d37e17beff29c8dc345f0cecef5ec840a0b
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-07-24 19:34:12 -0400

    sql stuff

--- includes/reservation.inc.php
+++ includes/reservation.inc.php
@@ -12,12 +12,13 @@ if(isset($_POST['reservation_submit'])){
     $phonenum = $_POST['reservation_phonenumber'];
     $comments = $_POST['reservation_questions_comments'];
 
-    $sql = "INSERT INTO rooms(`room_name`, `room_desc`, `room_capacity`) VALUES ($room,'description',$num_people);
+    $sql = "INSERT INTO rooms(`room_name`, `room_desc`, `room_capacity`) VALUES ($room,'description',$num_people);";
     
-            INSERT INTO reservations (`name`, `email`, `room`, `num_people`,`reservation_time`, `reservation_date`,`phone_number`,`comments_questions`)
-            VALUES ($name,$email,$room,$num_people,$time,$date,$phonenum,$comments);
+     $sql2 = "INSERT INTO reservations (`name`, `email`, `room`, `num_people`,`reservation_time`, `reservation_date`,`phone_number`,`comments_questions`)
+            VALUES ($name,$email,$room,$num_people,$time,$date,$phonenum,$comments);";"
             )";
     mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql2);
 
     header("Location: ../reservation.php?reservation=success");
 
