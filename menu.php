b99360b4b1a4bd1afc45b4bf6777f17f7a828159
Stephanie M. Tea <st893@cairn.edu>
2020-07-24 19:55:00 -0400

    test commit

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@ include_once "./includes/header.php";
     <h2 class="page-header">Menu</h2>
     <div class="sidenav">
       <a href="#">All Items</a>
-      <a href="#">Breakfast</a>
+      <a href="#">Breakfasts</a>
       <a href="#">Lunch </a>
       <a href="#">Dinner</a>
       <a href="#">Sides</a>
