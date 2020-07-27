bfbb77fad81be949a3556263f2234942995d8e45
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-07-27 11:25:43 -0400

    putin the sql update tables

--- sql/rooms-table.sql
+++ sql/rooms-table.sql
@@ -4,3 +4,6 @@ CREATE TABLE rooms(
     room_desc VARCHAR(255) NOT NULL,
     room_capacity TINYINT NOT NULL
 );
+/*INSERT INTO `rooms`(`room_name`, `room_desc`, `room_capacity`) VALUES ('40 seat','Our largest party room that can fit 40 people','40');*/
+-- INSERT INTO `rooms`(`room_name`, `room_desc`, `room_capacity`) VALUES ('Newly Wed','Special room for newly weds and other friends and family','6');
+-- INSERT INTO `rooms`(`room_name`, `room_desc`, `room_capacity`) VALUES ('15 seat','Smaller room that can hold 15 people',15);
