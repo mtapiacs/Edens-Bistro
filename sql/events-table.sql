42ae2c588a26ecafe2f2838ba6d5ea33d365b773
PNGYurei <jonjon.wrightpng@gmail.com>
2020-08-04 17:19:53 -0400

    Updated Event_table
    Added event_end_date

--- sql/events-table.sql
+++ sql/events-table.sql
@@ -3,5 +3,8 @@ CREATE TABLE events (
     event_name VARCHAR(100) NOT NULL,
     event_desc VARCHAR(255) NOT NULL,
     event_time TIME NOT NULL,
-    event_date DATE NOT NULL
+    event_start_date DATE NOT NULL,
+    event_end_date DATE NOT NULL
 );
+
+--INSERT INTO events ('event_name', 'event_desc', 'event_time', 'event_date') VALUES ('Free dessert with RCMH ticket', 'Free dessert for anyone who presents a Radio City Music Hall ticket', '00:00:00', '')
