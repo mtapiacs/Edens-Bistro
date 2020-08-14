e2a1ed6e9c38f5162110da73c6822ea1f9a7fe07
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-08-14 17:44:46 -0400

    changes

--- reservation-manage.php
+++ reservation-manage.php
@@ -5,7 +5,9 @@ include_once "./includes/header.php";
 <main class="main-container">
     
 <div class = "reservation-container">
+
     <?php
+
 
         $sql = "SELECT * FROM reservations ORDER BY reservation_id";
         $results = mysqli_query($conn,$sql);
@@ -19,24 +21,24 @@ include_once "./includes/header.php";
                             <table class = 'manage-res-table'>
                                 <tr class = 'res-tr'>
                                     <th class = 'res-th'>ID:</th>
-                                    <th>Name:</th>
-                                    <th>Room:</th>
-                                    <th>Number of People:</th>
-                                    <th>Reservation Time:</th>
-                                    <th>Reservation Date:</th>
-                                    <th>Phone Number:</th>
-                                    <th>Comments:</th>
+                                    <th class = 'res-th'>Name:</th>
+                                    <th class = 'res-th'>Room:</th>
+                                    <th class = 'res-th'>Guests:</th>
+                                    <th class = 'res-th'>Time:</th>
+                                    <th class = 'res-th'>Date:</th>
+                                    <th class = 'res-th'>Phone Number:</th>
+                                    <th class = 'res-th'>Comments:</th>
 
                                 </tr>
                                 <tr class = 'res-tr'>
-                                    <td><h5 class = 'res-records'>".$row['reservation_id']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['name']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['room']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['num_people']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['reservation_time']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['reservation_date']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['phone_number']."</h5></td>
-                                    <td><h5 class = 'res-records'>".$row['comments_questions']."</h5></td>
+                                    <td class = 'res-td'><p class = 'res-records'>".$row['reservation_id']."</p></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['name']."</h5></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['room']."</h5></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['num_people']."</h5></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['reservation_time']."</h5></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['reservation_date']."</h5></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['phone_number']."</h5></td>
+                                    <td class = 'res-td'><h5 class = 'res-records'>".$row['comments_questions']."</h5></td>
                                 </tr>
                             </table>
                 </div>";
