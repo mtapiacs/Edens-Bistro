a1913ba5fdbb8f9c11ea97de8f602e6fce7c7753
Miguel Tapia <mtapiafdez@gmail.com>
2020-08-13 20:37:31 -0400

    Missed one admin field

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$isAdmin = isset($_SESSION["isAdmin"]) ? true : false;
+$isAdmin = $_SESSION["isAdmin"];
 $isLoggedIn = isset($_SESSION["userId"])
 
 ?>
